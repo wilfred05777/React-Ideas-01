2416ef85e877c41c8edc07c07d6bd1c1b88853f8
Wilfred <wilfred5777@gmail.com>
2022-03-06 10:17:18 +0800

    Tasks

---
+++ src/components/Tasks.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const tasks = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: "Feb 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at School",
+    day: "March 5th at 3:30pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shopping",
+    day: "April 5th at 1:30pm",
+    reminder: false,
+  },
+];
+
+const Tasks = () => {
+  return <div></div>;
+};
+
+export default Tasks;
