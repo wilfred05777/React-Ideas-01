eb34c11f80b8727cb1bd8b4f3557622aba6160f0
Wilfred <wilfred5777@gmail.com>
2022-03-06 10:40:17 +0800

    setState hook to make it reusable all over the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "March 5th at 3:30pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "April 5th at 1:30pm",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       {/* <h1> Task Tracer App</h1> */}
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks} />
     </div>
   );
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 //// outside of the component state
 // const tasks = [
@@ -22,28 +22,28 @@ import { useState } from "react";
 //     },
 //   ];
 
-const Tasks = () => {
+const Tasks = ({ tasks }) => {
   //// making it part of the component state
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Doctors Appointment",
-      day: "Feb 5th at 2:30pm",
-      reminder: true,
-    },
-    {
-      id: 2,
-      text: "Meeting at School",
-      day: "March 5th at 3:30pm",
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: "Food Shopping",
-      day: "April 5th at 1:30pm",
-      reminder: false,
-    },
-  ]);
+  //   const [tasks, setTasks] = useState([
+  //     {
+  //       id: 1,
+  //       text: "Doctors Appointment",
+  //       day: "Feb 5th at 2:30pm",
+  //       reminder: true,
+  //     },
+  //     {
+  //       id: 2,
+  //       text: "Meeting at School",
+  //       day: "March 5th at 3:30pm",
+  //       reminder: true,
+  //     },
+  //     {
+  //       id: 3,
+  //       text: "Food Shopping",
+  //       day: "April 5th at 1:30pm",
+  //       reminder: false,
+  //     },
+  //   ]);
   return (
     <>
       {tasks.map((task) => (
