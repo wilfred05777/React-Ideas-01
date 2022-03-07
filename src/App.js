e8b398b04f0633e86de4adcb72d97e2981161c4e
Wilfred <wilfred5777@gmail.com>
2022-03-07 09:58:55 +0800

    onToggle bool on React without UI works good

--- src/App.js
+++ src/App.js
@@ -32,7 +32,12 @@ const App = () => {
 
   // // Toggle Reminder
   const toggleReminder = (id) => {
-    console.log(id);
+    // console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
